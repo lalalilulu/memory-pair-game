18f6467a0be751fde1610ab724c7120208310532
Anna Krivopusk <anna.krivopusk@t-systems.com>
2020-12-15 14:35:38 +0300

    Simplify game launch

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-//start the game
 (function launchApp() {
-    const gameBoard = document.querySelector(".memory-game");
-    const fragment = document.createElement("div");
-    createImgArray().map(img => createCard(img)).forEach(card => fragment.appendChild(card));
-    gameBoard.innerHTML = fragment.innerHTML;
+    const cards = document.querySelectorAll(".memory-card");
+    cards.forEach(card => {
+        let position = Math.floor(Math.random() * 20);
+        card.style.order = position;
+    });
+    cards.forEach(card => card.addEventListener("click", flipCard));
 })();
-
-function createImgArray() {
-    const imgArray = [];
-    for (let i = 1; i < 11; i++) {
-        imgArray.push("kitty-" + i);
-    }
-    imgArray.push(...imgArray);
-    return imgArray;
-}
-
-function createCard(image) {
-    const divCard = createElementWithClass("div", "memory-card");
-    divCard.dataset.image = image;
-    const imgFrontCard = createElementWithClass("img", "front");
-    imgFrontCard.setAttribute("src", "img/" + image + ".png");
-    const imgBackCard = createElementWithClass("img", "back");
-    imgBackCard.setAttribute("src", "img/back.png");
-    divCard.append(imgFrontCard, imgBackCard);
-    divCard.style.order = Math.floor(Math.random() * 100);
-    return divCard;
-}
-
-function createElementWithClass(tagName, cssClass) {
-    const element = document.createElement(tagName);
-    element.classList.add(cssClass);
-    return element;
-}
-
-const cards = document.querySelectorAll(".memory-card");
-console.log(cards);
-cards.forEach(card => card.addEventListener("click", flipCard));
 
 let lockBoard = false;
 let hasFlipped = false;
